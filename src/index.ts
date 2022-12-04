2585fd8fa58b2e555526f7947323fc27f7bbd333
Dylan <dylan.reeder5@gmail.com>
2022-12-04 13:11:30 -0500

    wip: server chat socket

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { Server } from 'socket.io';
 import cors from 'cors';
 import { createClient } from '@supabase/supabase-js';
 import userRouter from './routes/users';
@@ -12,9 +13,23 @@ export const supabase = createClient(
 const app = express();
 app.use(express.json({ limit: '200mb' }));
 app.use(cors());
+
 app.use('/', userRouter);
 app.use('/', authRouter);
 
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
+const io = new Server(
+  app.listen(process.env.PORT || 3000, () => {
+    console.log('Server is running on http://localhost:3000');
+  })
+);
+
+io.on('connection', function (socket) {
+  console.log('user connected');
+  socket.on('chat message', function (msg) {
+    io.emit('chat message', msg);
+    console.log('message: ' + msg);
+  });
+  socket.on('disconnect', function () {
+    console.log('user disconnected');
+  });
 });
